c5a9db8bc2cb13bbc30ae966db54ea71b33cb6a7
ottor <ottor@qq.com>
2017-10-24 10:49:16 +0800

    初始化项目

---
+++ nbzb/nbzb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nbzb
+//
+//  Created by 杨江平 on 2017/10/24.
+//  Copyright © 2017年 NBGA Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
